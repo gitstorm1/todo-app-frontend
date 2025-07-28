df9aec12bd43bd2dcbbebf00deb96c1f52279533
gitstorm1 <56407595+gitstorm1@users.noreply.github.com>
2025-07-28 15:39:28 +0500

    Add script to add/remove simple todo from unordered list

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,36 @@
-console.log("Hello from script.js! Your Todo app is starting.");
+/** @type {HTMLButtonElement} */
+const createTodo = document.getElementById("createTodo");
+/** @type {HTMLInputElement} */
+const createTodoName = document.getElementById("createTodoName");
+/** @type {HTMLUListElement} */
+const todoList = document.getElementById("todoList");
+
+createTodo.addEventListener("click", () => {
+    const todoName = createTodoName.value;
+
+    createTodoName.value = ""
+
+    if (todoName == "") {
+        alert("Todo name cannot be empty!");
+        return;
+    }
+
+    /** @type {HTMLLIElement} */
+    const todo = document.createElement("li");
+
+    /** @type {HTMLButtonElement} */
+    const completeTodo = document.createElement("button");
+    completeTodo.classList.add("complete-todo-button");
+    completeTodo.append("Complete");
+
+    completeTodo.addEventListener("click", () => {
+        todo.remove();
+    });
+
+    todo.append(todoName);
+    todo.append(completeTodo);
+
+    todoList.append(todo);
+
+    createTodoName.focus();
+});
